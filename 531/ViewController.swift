4cc8d2a88e26981a1c46f92e9ad49a0759655498
Nick P Smith <npsmith1990@gmail.com>
2015-12-25 11:47:08 -0600

    Some more brainstorming and planning

--- 531/ViewController.swift
+++ 531/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var bench: UInt!
+    private var squat: UInt!
+    private var dead: UInt!
+    private var ohp: UInt!
+    private var total4: UInt!
+    private var big3: UInt!
+    
+    @IBOutlet weak var benchInput: UITextField!
+    @IBOutlet weak var squatInput: UITextField!
+    @IBOutlet weak var deadliftInput: UITextField!
+    @IBOutlet weak var ohpInput: UITextField!
+    
+    @IBOutlet weak var total: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +33,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func pushWorkout(sender: AnyObject) {
+        print(benchInput.text!)
+        bench = UInt(benchInput.text!)
+        total.text = benchInput.text!
+    }
 
 }
 
